f6d23d86a1371196d1edb15ab33ddf561f250771
Evol <kovics.gergo@gmail.com>
2019-08-20 13:03:56 +0200

    First Commit

---
+++ src/main/java/com/mylightshare/main/com/mylightshare/main/controller/Controller.java
@@ -0,0 +1,14 @@
+package com.mylightshare.main.com.mylightshare.main.controller;
+
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@org.springframework.stereotype.Controller
+public class Controller {
+
+    @GetMapping("/")
+    public String index(Model model) {
+        return "index";
+    }
+}
---
+++ src/main/java/com/mylightshare/main/com/mylightshare/main/rest/RestController.java
@@ -0,0 +1,11 @@
+package com.mylightshare.main.com.mylightshare.main.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@org.springframework.web.bind.annotation.RestController
+@RequestMapping("/rest")
+public class RestController {
+
+    //TODO implement rest controller
+
+}
